ffdd7abc718433f61f3dc87e2a7af608b92d2ab2
mkaleem742-alt <mkaleem742@gmail.com>
2026-06-29 23:27:08 +0530

    Add date filters to Eleventy configuration
    Added filters for ISO and readable date formats.

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,24 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
 
+  eleventyConfig.addFilter("isoDate", (dateObj) => {
+    try {
+      return new Date(dateObj).toISOString().split('T')[0];
+    } catch(e) {
+      return "";
+    }
+  });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    try {
+      const d = new Date(dateObj);
+      const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+      return d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+    } catch(e) {
+      return "";
+    }
+  });
+
   return {
     dir: {
       input: ".",
