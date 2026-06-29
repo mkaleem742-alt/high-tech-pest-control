5d15142fd47bb9003c5b098f84ab5352ae5ec4f5
mkaleem742-alt <mkaleem742@gmail.com>
2026-06-29 23:13:52 +0530

    Update Eleventy config to remove ignored files
    Removed ignored files from Eleventy configuration.

--- .eleventy.js
+++ .eleventy.js
@@ -4,16 +4,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
 
-  // Inhe abhi build se ignore karo (baad mein theek karenge)
-  eleventyConfig.ignores.add("post.njk");
-  eleventyConfig.ignores.add("blog.njk");
-  eleventyConfig.ignores.add("_posts/**");
-
   return {
     dir: {
       input: ".",
       includes: "_includes",
       output: "_site"
-    }
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
   };
 };
