79fe9ccb56a4b03002fb591e2ad65619c6381723
mkaleem742-alt <mkaleem742@gmail.com>
2026-06-29 22:57:06 +0530

    Remove date filters and add file ignores
    Removed date filters and added ignores for specific files.

--- .eleventy.js
+++ .eleventy.js
@@ -4,23 +4,16 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("images");
 
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
-    const d = new Date(dateObj);
-    const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-    return `${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}`;
-  });
-
-  eleventyConfig.addFilter("isoDate", (dateObj) => {
-    return new Date(dateObj).toISOString().split('T')[0];
-  });
+  // Inhe abhi build se ignore karo (baad mein theek karenge)
+  eleventyConfig.ignores.add("post.njk");
+  eleventyConfig.ignores.add("blog.njk");
+  eleventyConfig.ignores.add("_posts/**");
 
   return {
     dir: {
       input: ".",
       includes: "_includes",
       output: "_site"
-    },
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
+    }
   };
 };
